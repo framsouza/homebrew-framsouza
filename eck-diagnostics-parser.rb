3df53f5e604321de2e3e6bde8cbd06fa5a502451
framsouza <fram.souza14@gmail.com>
2022-07-27 17:38:09 +0200

    Brew formula update for eck-diagnostics-parser version v1.0.1

--- eck-diagnostics-parser.rb
+++ eck-diagnostics-parser.rb
@@ -5,11 +5,11 @@
 class EckDiagnosticsParser < Formula
   desc ""
   homepage "https://github.com/framsouza/eck-diagnostics-parser"
-  version "1.0.0"
+  version "1.0.1"
 
   on_macos do
-    url "https://github.com/framsouza/eck-diagnostics-parser/releases/download/v1.0.0/eck-diagnostics-parser_1.0.0_darwin_all.tar.gz"
-    sha256 "d4bdbe8f99232e428c2d2d38eaeb8542a6884f6c930efe803c1be93cc16c7132"
+    url "https://github.com/framsouza/eck-diagnostics-parser/releases/download/v1.0.1/eck-diagnostics-parser_1.0.1_darwin_all.tar.gz"
+    sha256 "d4a47f479da6a2c5dbc926d1f15abbaec6d41b7e78c28d91fb3b37df50f02f5f"
 
     def install
       bin.install "eck-diagnostics-parser"
@@ -18,16 +18,16 @@ class EckDiagnosticsParser < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/framsouza/eck-diagnostics-parser/releases/download/v1.0.0/eck-diagnostics-parser_1.0.0_linux_arm64.tar.gz"
-      sha256 "6d964607d2384f4458c13d7c17d8a12762269d370174a640169636013373c960"
+      url "https://github.com/framsouza/eck-diagnostics-parser/releases/download/v1.0.1/eck-diagnostics-parser_1.0.1_linux_arm64.tar.gz"
+      sha256 "606b34c5379c0afc3e92a2b5a09f8b8ecb8b4897300a3359f166c8e141a57836"
 
       def install
         bin.install "eck-diagnostics-parser"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/framsouza/eck-diagnostics-parser/releases/download/v1.0.0/eck-diagnostics-parser_1.0.0_linux_amd64.tar.gz"
-      sha256 "9f66f3d2f59574afe595a3df2cdbdf1554545aff3a4f0f1c184a11e64726932c"
+      url "https://github.com/framsouza/eck-diagnostics-parser/releases/download/v1.0.1/eck-diagnostics-parser_1.0.1_linux_amd64.tar.gz"
+      sha256 "f1a4fcae3b3034c0b594b1403ddc98592ab5508beb54e3011747e903a6f44d6f"
 
       def install
         bin.install "eck-diagnostics-parser"
